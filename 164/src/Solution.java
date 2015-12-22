37ef5202aa9580b19a7f77f12ef1fa506d85033d
langruixiang <langruixiang@foxmail.com>
2015-12-22 22:48:17 +0800

    modify 164

--- 164/src/Solution.java
+++ 164/src/Solution.java
@@ -1,40 +1,53 @@
 public class Solution {
+	private class Bucket{
+		int size;
+		int min;
+		int max;
+		
+		public void add(int num){
+			if(size == 0){
+				min = num;
+				max = num;
+			}else{
+				min = min < num ? min : num;
+				max = max > num ? max : num;
+			}
+			size++;
+		}
+	}
+	
     public int maximumGap(int[] nums) {
     	if(nums == null || nums.length < 2){
     		return 0;
     	}
     	
-    	boolean []flag = new boolean[Integer.MAX_VALUE];
-    	boolean hasZero = false;
+    	int min = nums[0];
+    	int max = nums[0];
+    	
+    	for(int i = 1; i < nums.length; i++){
+    		min = nums[i] < min ? nums[i] : min;
+    		max = nums[i] > max ? nums[i] : max;
+    	}
+    	
+    	int len = (int)Math.ceil(((double)(max - min)) / (nums.length - 1));
+    	len = len < 1 ? 1 : len;
+    	
+    	Bucket[] bucket = new Bucket[nums.length];
+    	for(int i = 0; i < nums.length; i++){
+    		bucket[i] = new Bucket();
+    	}
     	
     	for(int i = 0; i < nums.length; i++){
-    		if(nums[i] != 0){
-    			flag[nums[i] - 1] = true;
-    		}else{
-    			hasZero = true;
-    		}
+    		bucket[(nums[i] - min) / len].add(nums[i]);
     	}
     	
-    	int indexPre = -1;
-    	int i = 0;
-    	if(!hasZero){
-        	while(flag[i] == false){
-        		i++;
-        	}
-        	
-        	indexPre = i;
-        	i++;    		
-    	}
-    	
-    	int ret = Integer.MIN_VALUE;
-    	
-    	while(i < flag.length){
-    		if(flag[i] == true){
-    			ret = i - indexPre > ret ? i - indexPre : ret;
-    			indexPre = i;
+    	int preMax = bucket[0].max;
+    	int ret = 0;
+    	for(int i = 1; i < nums.length; i++){
+    		if(bucket[i].size != 0){
+    			ret = ret > bucket[i].min - preMax ? ret : bucket[i].min - preMax;
+    			preMax = bucket[i].max;
     		}
-    		
-    		i++;
     	}
         
     	return ret;
